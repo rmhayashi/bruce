d1a89146a5a80a469bb8a8177bb5b42bbf1fb211
Marco A. Rocha <49138259+marcoarocha1994@users.noreply.github.com>
2019-06-18 17:53:10 -0300

    Update xmlgen.py

--- xmlgen.py
+++ xmlgen.py
@@ -4,4 +4,4 @@ xmlgen = Blueprint('webservices', __name__,url_prefix="/webservices")
 
 @xmlgen.before_request
 def before_request():
-current_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bases/bruce.sqlite3'
+  current_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bases/bruce.sqlite3'
