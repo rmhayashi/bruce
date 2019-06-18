02f623e6647509af1b3af7c654ecd3dc64870d4f
Marco A. Rocha <49138259+marcoarocha1994@users.noreply.github.com>
2019-06-18 17:52:50 -0300

    Update and rename xmlgenerator.py to xmlgen.py

---
+++ xmlgen.py
@@ -0,0 +1,7 @@
+from classes import *
+
+xmlgen = Blueprint('webservices', __name__,url_prefix="/webservices")
+
+@xmlgen.before_request
+def before_request():
+current_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bases/bruce.sqlite3'
