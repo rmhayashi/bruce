a475efb5f696916a7cd482c0ca764ce45a89d3d2
Marco A. Rocha <49138259+marcoarocha1994@users.noreply.github.com>
2019-06-19 10:57:39 -0300

    Adiconada função de identação
    indent() irá realizar a formatação do xml

--- classes.py
+++ classes.py
@@ -117,6 +117,21 @@ class XML(object):
         self.productTopologyCategory = productTopologyCategory
         self.portabilityCode = portabilityCode
         self.serviceOrderNumber = serviceOrderNumber
+        
+    def indent(elem, level=0):
+        i = "\n" + level * "  "
+        if len(elem):
+            if not elem.text or not elem.text.strip():
+                elem.text = i + "  "
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+            for elem in elem:
+                indent(elem, level + 1)
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+        else:
+            if level and (not elem.tail or not elem.tail.strip()):
+                elem.tail = i
 
     # WebServices do sigres
 
